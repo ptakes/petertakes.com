924ca07d5bd59006c299c7dd0efd40faaeaf8c31
Peter Takes <Peter.Takes@sopheon.com>
2016-11-21 11:50:24 +0100

    Added build config file.

---
+++ build/config.ts
@@ -0,0 +1,3 @@
+import { join } from 'path';
+
+export const TASKS_DIR = join(process.cwd(), 'build', 'tasks');
--- gulpfile.ts
+++ gulpfile.ts
@@ -1,6 +1,7 @@
+import { TASKS_DIR } from './build/config';
 import * as gulp from 'gulp';
 import * as help from 'gulp-help-four';
 import * as requireDir from 'require-dir';
 
-requireDir('./build/tasks');
+requireDir(TASKS_DIR);
 help(gulp);
